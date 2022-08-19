def4619f7073dfa44974eca104a15e55fef72775
Vivek Vajpai <vivekvajpai65@gmail.com>
2022-08-19 17:56:22 +0530

    Add delete profile and all associated articles(if any) option to nav_bar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-	before_action :set_user, only: [:show, :edit, :update]
+	before_action :set_user, only: [:show, :edit, :update, :destroy]
 	before_action :require_user, only: [:edit, :update]
 	before_action :require_same_user, only: [:edit, :update, :destroy]
 
@@ -36,6 +36,13 @@ class UsersController < ApplicationController
 		else
 			render 'new'
 		end
+	end
+
+	def destroy
+		@user.destroy
+		session[:user_id] = nil
+		flash[:notice] = "Account and all associated artilces successfully deleted"
+		redirect_to root_path
 	end
 
 	private
