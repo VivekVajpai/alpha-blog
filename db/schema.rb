bf5f24e39a9b58a51b4a0313e307068759e27057
Vivek Vajpai <vivekvajpai65@gmail.com>
2022-08-19 18:31:31 +0530

    Add migration file for admin to users and assigned admin from back end

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_18_115905) do
+ActiveRecord::Schema.define(version: 2022_08_19_124622) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2022_08_18_115905) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
